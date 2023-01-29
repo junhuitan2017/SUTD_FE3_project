020fb40cb9a2fd9227b448d1e321dd7a64c74d4e
Tan Jun Hui <akimotojh@gmail.com>
2023-01-29 16:43:18 +0800

    Fix lint issues

--- src/components/HowTo.tsx
+++ src/components/HowTo.tsx
@@ -6,12 +6,17 @@ const HowTo = (): ReactElement => {
             <h2 className="board-word">How To Play</h2>
             <article className="howto">
                 <p>
-                    The aim of the game is to turn off all the lights on the board.<br/>
-                    When you click on a light, it will toggle on/off along with its 4 neighbours (N,S,E,W).
+                    The aim of the game is to turn off all the lights on the
+                    board
+                    <br />
+                    When you click on a light, it will toggle on/off along with
+                    its 4 neighbours (N,S,E,W)
+                    <br />
+                    When you are bored, press &quot;OUT&quot;
                 </p>
             </article>
         </section>
-    )
+    );
 };
 
 export default HowTo;
